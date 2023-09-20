d0e1d982c8d5bb0655e0e23c3b8403b7e5d1af2b
Gleb Musin <glebmusin77@gmail.com>
2023-09-21 00:02:06 +0500

    Initialized config, mongo db connection

--- backend/cmd/comparison_center/main.go
+++ backend/cmd/comparison_center/main.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/Unlites/backend/comparison_center/config"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+	cfg, err := config.NewConfig()
+	if err != nil {
+		log.Fatalf("failed to init config: %v", err)
+	}
+
+	ctx := context.Background()
+
+	log.Fatal(cfg.MongoURI)
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(cfg.MongoURI))
+	if err != nil {
+		log.Fatalf("failed to connect to mongodb: %v", err)
+	}
+
+	if err := client.Ping(ctx, nil); err != nil {
+		log.Fatal(err)
+	}
+
+}
