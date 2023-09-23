d508ee3c1c5101f7e4b2ef04fa6fce5d8fd0b0c6
Gleb Musin <glebmusin77@gmail.com>
2023-09-23 18:22:28 +0500

    Added http server configs

--- backend/config/config.go
+++ backend/config/config.go
@@ -5,16 +5,27 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"time"
 
 	"github.com/ilyakaznacheev/cleanenv"
 )
+
+type HttpServer struct {
+	Address         string        `yaml:"address"`
+	ReadTimeout     time.Duration `yaml:"read_timeout"`
+	WriteTimeout    time.Duration `yaml:"write_timeout"`
+	IdleTimeout     time.Duration `yaml:"idle_timeout"`
+	ShutdownTimeout time.Duration `yaml:"shutdown_timeout"`
+}
 
 type DB struct {
 	MongoURI string `env:"MONGODB_URI" env-required:"true"`
 }
 
 type Config struct {
+	HttpServer `yaml:"http_server"`
 	DB
+	PhotosDir string `env:"PHOTOS_DIR" env-required:"true"`
 }
 
 func NewConfig() (*Config, error) {
