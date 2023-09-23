bd3ecfbf12cc78282c3cdbaa9388e50a604f952a
Gleb Musin <glebmusin77@gmail.com>
2023-09-23 18:24:34 +0500

    Added comparison handler

--- backend/internal/comparison/delivery/http/v1/router.go
+++ backend/internal/comparison/delivery/http/v1/router.go
@@ -1,1 +1,27 @@
 package v1
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/render"
+)
+
+type ComparisonHandler struct {
+	Router http.Handler
+	// uc domain.ComparisonUsecase
+}
+
+func NewComparisonHandler() *ComparisonHandler {
+	router := chi.NewRouter()
+
+	router.Get("/ping", ping)
+
+	return &ComparisonHandler{Router: router}
+}
+
+func ping(w http.ResponseWriter, r *http.Request) {
+	render.JSON(w, r, map[string]interface{}{
+		"message": "pong from comparison handler",
+	})
+}
